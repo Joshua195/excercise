39a300330113e1ecdb67b55a1ad91c813d9e5204
Josue Gonzalez <gojoshua195@gmail.com>
2020-10-29 07:29:33 -0600

    add user logic to sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ROUTES} from '../../consts/routes';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,10 +10,13 @@ import {ROUTES} from '../../consts/routes';
 export class SidebarComponent implements OnInit {
   public routes: typeof ROUTES = ROUTES;
   public isOpenUiElements = false;
+  public user;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => this.user = user);
+  }
 
   public openUiElements(): void {
     this.isOpenUiElements = !this.isOpenUiElements;
